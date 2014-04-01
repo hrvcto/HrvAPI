e0a2f4d99fb1b5e4a583834871dccef4bd70ab66
Hrv CTO <hrvcto@gmail.com>
2014-04-01 10:49:19 +0800

    Add id to each item of loadCzml arguments.

--- CesiumAPI/api.loadCzml.js
+++ CesiumAPI/api.loadCzml.js
@@ -1,18 +1,20 @@
 define(function(){
     function loadCzml(cesiumWidget, options) { 
         var timer = setInterval(function() {
-            var url = options[0];
+            var czmlItem = options[0];
+            console.log("czmlItem:"+JSON.stringify(czmlItem));
             var czmlDataSource = new Cesium.CzmlDataSource();
-            console.log(url);
-            czmlDataSource.loadUrl(url).then(function() {
+            console.log(czmlItem.url);
+            czmlDataSource.loadUrl(czmlItem.url).then(function() {
                 cesiumWidget.dataSources.add(czmlDataSource);
             })
             options = options.slice(1, options.length);
+            console.log("options:"+JSON.stringify(options));
             if (options.length<=0) {
                 clearInterval(timer);
             }
             require(['CesiumAPI/api.idManager.js'], function(){
-                window.idManager.addObject(url, czmlDataSource);
+                window.idManager.addObject(czmlItem.id, czmlDataSource);
             });
         }, 0);
     }
