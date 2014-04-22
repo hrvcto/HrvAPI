8a48678379c4e8d4dcc533af70e0a01cd2727e9e
liuda <liu.da101@gmail.com>
2014-04-22 20:20:30 +0800

    add tips to addBillboard

--- CesiumAPI/api.addBillboard.js
+++ CesiumAPI/api.addBillboard.js
@@ -5,6 +5,7 @@ define(['./api.utils.js'], function(utils){
     var ellipsoid = cesiumWidget.centralBody.ellipsoid;
     
     var position = options.position;
+    var tips = options.tips;
 
     if(typeof position.longitude == 'string'){
       position.longitude = utils.ConvertDMStoDD(position.longitude);
@@ -32,6 +33,34 @@ define(['./api.utils.js'], function(utils){
         var id = options.id || window.idManager.nextID();
         window.idManager.addObject(id, billboards, scene.primitives);
       });
+
+
+      if(tips){
+        var mouseHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+
+        var label = document.createElement('div');
+        label.style.position = 'absolute';
+        label.style.backgroundColor = 'rgba(255,255,255,0.8)';
+        label.style.color = 'rgba(0,0,0,1)';
+        label.style.fontSize = '14px';
+        label.style.padding = '3px';
+        label.style.lineHeight = '24px';
+        label.innerHTML = tips;
+
+        mouseHandler.setInputAction(function(data){
+          var pickedObject = scene.pick(data.endPosition);
+          
+          if(pickedObject && pickedObject.primitive == b){
+            label.style.top = data.endPosition.y + 10 + 'px';
+            label.style.left = data.endPosition.x + 10 + 'px';
+            scene.canvas.parentNode.insertBefore(label);
+          } else {
+            try{
+              scene.canvas.parentNode.removeChild(label);
+            }catch(e){}
+          }
+        }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+      }
     };
 
     image.src = options.url;
